e109b1e9a60827f00b058c9ce1ec4f8f3923034a
pendenzajr <pendenzajr@gmail.com>
2024-09-06 21:19:52 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+function pesquisar() {
+
+    // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    
+    campoPesquisa = campoPesquisa.toLowerCase();
+    
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+    let nome = "";
+    let olfativa = "";
+    let tags = "";
+
+    // se campo de pesquisa for vazio, nao retorna nada
+    if(campoPesquisa == "") {
+        section.innerHTML = "<h2>Digite alguma coisa no campo de busca</h2>"
+        return             
+    }
+ 
+    // Itera sobre cada perfume nos dados
+    for (let parfum of dados) {    
+        nome = parfum.nome.toLowerCase();
+        olfativa = parfum.olfativa.toLowerCase();
+        tags= parfum.tags.toLowerCase();   
+        // se titulo icludes campoPesquisa 
+        if(nome.includes(campoPesquisa) || olfativa.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+
+        // Cria uma nova div para cada perfume e adiciona as informações relevantes
+        resultados += `
+        <div class="item-resultado">
+            <h2>
+                <p class="descricao-meta">${parfum.nome}</p>
+            </h2>
+            <p class="descricao-meta">${parfum.marca + " " + parfum.lancamento}</p>
+            <p class="descricao-meta">${parfum.tipo}</p>
+            <p class="descricao-meta">${parfum.olfativa}</p>
+            <p class="descricao-meta">
+                <a href="${parfum.link}" target="_blank" title="Saiba mais" class="botao">Loja online</a>
+            </p>
+        </div>
+    `;
+        }
+        
+    }
+
+    if(!resultados) {
+        resultados = "<h2>Nada foi encontrado</h2>"
+    }
+
+    // Atribui a string com os resultados ao conteúdo HTML da seção
+    section.innerHTML = resultados;
+}
---
+++ dados.js
@@ -0,0 +1,38 @@
+let dados = [
+    {
+        nome: "Classic",
+        marca: "BöRR Perfumaria",
+        lancamento: "2022",
+        tipo: "Amadeirado Especiado Masculino",
+        olfativa: "As notas de topo são: Mandarina Sanguínea , Toranja e Hortelã. As notas de coração são: Canela, Notas Especiadas e Rosa. As notas de fundo são: Âmbar, Couro, Notas Amadeiradas e Patchouli Indiano.",
+        link: "https://www.borrperfumaria.com.br/perfumes/masculinos/borr-classic-100ml",
+        tags: "one milion um milhao 1 milion 1 million one million"
+    },
+    {
+        nome: "Elegance",
+        marca: "BöRR Perfumaria",
+        lancamento: "2022",
+        tipo: "Amadeirado Aromático Fougère Masculino",
+        olfativa: "As notas de topo são: Bergamota, Limão Siciliano e Grapefruit. As notas de coração são: Pimenta, Lavanda e Zimbro. As notas de fundo são: Âmbar, Vetiver e Cedro.",
+        link: "https://www.borrperfumaria.com.br/perfumes/masculinos/borr-elegance-100ml",
+        tags: "212 tu an tu dois um dois"
+      },
+      {
+        nome: "Chaos",
+        marca: "BöRR Perfumaria",
+        lancamento: "2024",
+        tipo: "Amadeirado Aromático Fougère Masculino",
+        olfativa: "As notas de topo são: Bergamota de Calabria, Pimenta e Elemi. As notas de coração são: Pimenta Rosa, Vetiver e Pimenta da Jamaica. As notas de fundo são: Âmbar Gris, Patchouli e Ládanum.",
+        link: "https://www.borrperfumaria.com.br/perfumes/masculinos/borr-chaos-100ml",
+        tags: "sauvage seuvagem sovage savage"
+      },
+      {
+        nome: "Midnight",
+        marca: "BöRR Perfumaria",
+        lancamento: "2023",
+        tipo: "Oriental Fougère Masculino",
+        olfativa: "As notas de topo são: Bergamota, Limão Siciliano e Mandarina. As notas de coração são: Lavanda, Jasmim e Cardamomo. As notas de fundo são: Fava Tonka, Âmbar, Patchouli e Cedro.",
+        link: "https://www.borrperfumaria.com.br/perfumes/masculinos/borr-midnight-100ml",
+        tags: "ferrari ferrari black cavalo preto"
+      }
+];
