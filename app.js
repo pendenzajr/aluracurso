71894f1156b5fe3909955fddd10909e07f32358e
pendenzajr <pendenzajr@gmail.com>
2024-09-14 17:27:14 -0300

    Ajustes campo pesquisa
    Foi adicionado alguns ajustes no campo de pesquisa, agora ele apresenta alguns textos alternados dentro do campo de pesquisa, deixando mais intuitivo

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ function pesquisar() {
   
     let resultados = "";
   
+    //Mensagem apresentada caso buscar com o campo em branco
     if (!campoPesquisa) {
       section.innerHTML = "<h2>Digite alguma coisa para pesquisar.</h2>";
       return;
     }
   
+    //Utiliza informações do dados.js e apresenta apos a pesquisa
     for (const parfum of dados) {
       const { nome, olfativa, tags, lancamento} = parfum;
       if (nome.match(regex) || olfativa.match(regex) || tags.match(regex) || lancamento.match(regex)) {
@@ -28,6 +30,41 @@ function pesquisar() {
   
     section.innerHTML = resultados || "<h2>Nenhum perfume encontrado para '" + campoPesquisa + "'.</h2>";
   }
+
+  
+//Apresenta textos aleatórios dentro do campo de pesquisa
+const placeholders = [
+    "Pesquise seu BöRR: Midnight",
+    "Pesquise pelo importado: 212",
+    "Pesquise por nota: Lavanda",
+    "Pesquise por Genero: Masculino"
+];
+
+const searchInput = document.getElementById("campo-pesquisa");
+let currentPlaceholderIndex = 0;
+let shownPlaceholders = [];
+
+function changePlaceholder() {
+    // Verifica se todos os placeholders já foram mostrados
+    if (shownPlaceholders.length === placeholders.length) {
+        shownPlaceholders = []; // Reinicia o array para começar novamente
+    }
+
+    // Encontra o próximo placeholder que ainda não foi mostrado
+    let nextIndex = currentPlaceholderIndex;
+    while (shownPlaceholders.includes(nextIndex)) {
+        nextIndex = (nextIndex + 1) % placeholders.length; // Incrementa e faz o módulo para garantir que fique dentro do array
+    }
+
+    currentPlaceholderIndex = nextIndex;
+    shownPlaceholders.push(nextIndex);
+
+    searchInput.placeholder = placeholders[nextIndex];
+}
+
+// Chamar a função pela primeira vez e depois a cada 5 segundos
+changePlaceholder();
+setInterval(changePlaceholder, 2000);
   
   // Adicionando o event listener para a tecla Enter
   const campoPesquisa = document.getElementById("campo-pesquisa");
